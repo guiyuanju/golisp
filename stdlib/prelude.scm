eee87824eafaf1b59af321517ed88610a691b5c3
guiyuanju <guiyuanjune@outlook.com>
2025-07-08 00:00:20 +0800

    add head, tail etc.

--- stdlib/prelude.scm
+++ stdlib/prelude.scm
@@ -4,23 +4,29 @@
 (macro or [a b]
     (list 'if a a b))
 
+(fn head [xs] (. 0 xs))
+(fn snd [xs] (. 1 xs))
+(fn tail [xs] (: 1 (len xs) xs))
+(fn init [xs] (: 0 -1 xs))
+(fn last [xs] (. -1 xs))
+
 (fn map [f xs]
     (if (= 0 (len xs))
         ()
-        (append (map f (: 0 -1 xs)) (f (. -1 xs)))))
+        (append (map f (init xs)) (f (last xs)))))
 
 (fn pair [seq]
     (if (= 0 (len seq))
         ()
-        (append (pair (: 0 -2 seq)) (list (. -2 seq) (. -1 seq)))))
+        (append (pair (: 0 -2 seq)) (list (. -2 seq) (last seq)))))
 
 (fn concat [xs ys]
     (if (= 0 (len ys))
         xs
-        (concat (append xs (. 0 ys)) (: 1 (len ys) ys))))
+        (concat (append xs (head ys)) (tail ys))))
 
 (macro let [bindings body]
-    (var vars (map (fn [x] (list 'var (. 0 x) (. 1 x)))
+    (var vars (map (fn [x] (list 'var (head x) (snd x)))
                     (pair bindings)))
     (list (concat (concat '(fn []) vars)
             (list body))))
